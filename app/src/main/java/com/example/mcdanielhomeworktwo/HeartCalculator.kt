2639e9fedaadfbc9eeecd4b94b42cc09d81d2720
putthepieceawaywalter <putthepieceawaywalter@gmail.com>
2020-07-07 22:34:22 -0700

    Added a Heart Calculator class, added a Metric toggle for distance input and updated the id's for the Edit Text fields

---
+++ app/src/main/java/com/example/mcdanielhomeworktwo/HeartCalculator.kt
@@ -0,0 +1,25 @@
+package com.example.mcdanielhomeworktwo
+
+class HeartCalculator {
+
+
+    /*
+        This takes in the following parameters
+        Heart Rate
+        Distance
+        Time
+        Tire Size
+
+        Returns the following
+        Total Wheel Revolutions
+            First get circumference in metric which is (622 + (Tire Size * 2)) * pi
+                If the user input distance in standard convert circumference to standard
+            Divide total distance by circumference of wheel
+        Total Heart Beats
+        Wheel Revolutions per heart beat
+
+
+
+
+     */
+}
