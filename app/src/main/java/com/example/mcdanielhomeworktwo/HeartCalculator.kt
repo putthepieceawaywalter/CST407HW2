d8b88d45bc9c4d914e21929d54bcfb64e7d0b81a
putthepieceawaywalter <putthepieceawaywalter@gmail.com>
2020-07-08 18:05:28 -0700

    app now works, scrapes data and outputs the correct value

--- app/src/main/java/com/example/mcdanielhomeworktwo/HeartCalculator.kt
+++ app/src/main/java/com/example/mcdanielhomeworktwo/HeartCalculator.kt
@@ -1,5 +1,7 @@
 package com.example.mcdanielhomeworktwo
 
+import android.widget.ToggleButton
+
 
 class HeartCalculator {
 
@@ -12,10 +14,16 @@ class HeartCalculator {
     fun calculateWheelRevolutions(circumference: Double, distance: Double) : Double {
         return distance / (circumference *.000001)
     }
-    fun totalHeartBeats(elapsedTime: Double, avgHeartBeat: Int): Int {
-        return ((elapsedTime * 60).toInt()) * avgHeartBeat
+    fun totalHeartBeats(elapsedTime: Double, heartRate: Int): Int {
+        return ((elapsedTime * 60).toInt()) * heartRate
     }
     fun wheelRevsPerHeartBeat(wheelRevolutions: Double, totalHeartBeats: Int) : Double {
         return wheelRevolutions / totalHeartBeats
+    }
+    fun calculateBeatsPerRev(distance: Double, tireSize: Int, heartRate: Int, elapsedTime: Double) : Double {
+
+        var revs = calculateWheelRevolutions(calculateWheelCircumference(tireSize), distance)
+        var beats = totalHeartBeats(elapsedTime, heartRate)
+        return wheelRevsPerHeartBeat(revs, beats)
     }
 }
--- app/src/main/java/com/example/mcdanielhomeworktwo/MainActivity.kt
+++ app/src/main/java/com/example/mcdanielhomeworktwo/MainActivity.kt
@@ -5,38 +5,99 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.ToggleButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 
 const val TIRE_SIZE_MIN = 20
 const val TIRE_SIZE_MAX = 127
-const val HEART_BEAT_MIN = 40
-const val HEART_BEAT_MAX = 200
+const val HEART_RATE_MIN = 40
+const val HEART_RATE_MAX = 200
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, TextWatcher {
+    private var distance: Double = 0.0
+    private var heartRate = 0
+    private var revolutions = 0
+    private var isMetric = false
+    private var elapsedTime = 0.0
+    private var tireSize = 0
+    private var result = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // metricSwitch.
+
+        // metricSwitch.
+        submit.setOnClickListener(this)
+        //toggleMetric.
+        editTxtDistance.addTextChangedListener(this)
+
+
+
 
 
     }
 
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+
+    override fun onClick(button: View?) {
+        when (button?.id) {
+            R.id.submit -> submit()
+
+        }
+    }
+    private fun submit()
+    {
+        distance = editTxtDistance.text.toString().toDouble()
+        heartRate = editTxtHeartRate.text.toString().toInt()
+        elapsedTime = editTxtTime.text.toString().toDouble()
+        tireSize = editTxtTireSize.text.toString().toInt()
+
+        val toggle: ToggleButton = findViewById(R.id.toggleMetric)
+        toggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+               // it is metric already!
+            } else {
+                distance = HeartCalculator().distanceInMilesToKM(distance)
+            }
+        }
+
+        if (heartRate > HEART_RATE_MAX) {
+            heartRate = HEART_RATE_MAX
+        } else if (heartRate < HEART_RATE_MIN) {
+            heartRate = HEART_RATE_MIN
+        }
+
+        if (tireSize > TIRE_SIZE_MAX) {
+            tireSize = TIRE_SIZE_MAX
+        } else if (tireSize < TIRE_SIZE_MIN)
+        {
+            tireSize = TIRE_SIZE_MIN
+        }
+
+
+
+
+        result = HeartCalculator().calculateBeatsPerRev(distance, tireSize, heartRate, elapsedTime)
+        txtResult.text = result.toString()
+
+
+
+
     }
 
     override fun afterTextChanged(s: Editable?) {
-        TODO("Not yet implemented")
     }
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        TODO("Not yet implemented")
+        if (editTxtDistance.text!!.isNotEmpty() && editTxtHeartRate.text!!.isNotEmpty() &&
+                editTxtTime.text!!.isNotEmpty()&& editTxtHeartRate.text!!.isNotEmpty()) {
+            submit()
+        }
+
     }
 }
